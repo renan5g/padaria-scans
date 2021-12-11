0f933afcbc8f75c1d08faf03a869657b97d892d0
nanzito <renansilva.dev5g@gmail.com>
2021-12-11 15:53:06 -0300

    feat: add theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,69 @@
+export const theme = {
+  colors: {
+    white: '#FFFFFF',
+    green: '#1B873F',
+    orange: '#FF7A29',
+    yellow: '#FFCD1E',
+    success: '#04D361',
+    alert: '#E83F5B',
+
+    heading: '#383842',
+    body: '#6C6C80',
+
+    bgColor: '#F0F0F5',
+
+    shapesBg: '#FFFFFF',
+    shapesLine: '#DCDDE0',
+
+    linear: 'linear-gradient(90deg, #f8941c 5%, #ffc43b 75%)',
+    shadow: '0 5px 25px rgba(0,0,0,0.05)',
+  },
+  font: {
+    family:
+      "Poppins, -apple-system, BlinkMacSystemFont, 'Segoe UI',Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
+    ligth: 300,
+    normal: 400,
+    bold: 600,
+  },
+  sizes: {
+    full: '100%',
+    8: '0.5rem',
+    12: '0.75rem',
+    16: '1rem',
+    24: '1.5rem',
+    32: '2rem',
+    40: '2.5rem',
+    48: '3rem',
+    56: '3.5rem',
+    64: '4rem',
+  },
+  space: {
+    10: '0.625rem',
+    20: '1.25rem',
+    40: '2.5rem',
+    80: '5rem',
+    160: '10rem',
+  },
+  radii: {
+    none: '0',
+    sm: '0.125rem', // 2px
+    base: '0.25rem', // 4px
+    md: '0.375rem', // 6px
+    lg: '0.5rem', // 8px
+    xl: '0.75rem', // 12px
+    '2xl': '1rem', // 16px
+    '3xl': '1.25rem', // 20px
+    full: '9999px',
+  },
+  zIndices: {
+    hide: -1,
+    auto: 'auto',
+    base: 0,
+    dropdown: 1000,
+    banner: 1200,
+    overlay: 1300,
+    modal: 1400,
+    toast: 1700,
+    tooltip: 1800,
+  },
+};
