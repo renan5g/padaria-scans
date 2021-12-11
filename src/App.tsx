727554bb6a029b4ae968589ea84bd5e6cccc3206
nanzito <renansilva.dev5g@gmail.com>
2021-12-11 16:08:40 -0300

    fix: add theme provider on app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { ThemeProvider } from 'styled-components';
+
+import { theme } from './styles/theme';
 import { GlobalStyles } from './styles/global';
 
 export const App = () => {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyles />
       <h1>Padaria Scans</h1>
-    </>
+    </ThemeProvider>
   );
 };
