3fc1ef1a4a5f3790e44824ff2d986f9f1023fe7c
nanzito <renansilva.dev5g@gmail.com>
2021-12-11 15:58:47 -0300

    feat: css global

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -6,4 +6,35 @@ export const GlobalStyles = createGlobalStyle`
 		padding: 0;
 		box-sizing: border-box;
 	}
+
+	html {
+    scroll-behavior: smooth;
+    box-sizing: inherit;
+    width: 100%;
+  }
+
+  // Scrollbar styles 
+  html {
+    scrollbar-width: thin;
+    scrollbar-color: ${({ theme }) => theme.colors.heading};
+  }
+
+  body::-webkit-scrollbar {
+    width: 6px;
+  }
+
+  body::-webkit-scrollbar-thumb {
+    background-color: ${({ theme }) => theme.colors.heading};
+    border-radius: 10px;
+  }
+
+  body {
+    font-family: ${({ theme }) => theme.font.family};
+    background-color: ${({ theme }) => theme.colors.bgColor};
+    color: ${({ theme }) => theme.colors.heading};
+  }
+	
+  ul, li, ol {
+    list-style: none;
+  }
 `;
